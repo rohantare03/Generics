f03089b868c71ea7b8b144c0d3884cd0b06e407b
rohantare03 <rohatare123@gmail.com>
2022-05-22 17:20:38 +0530

    [Rohan] Add : Finding Max Values

--- Generics/Generics/MaxNumber.cs
+++ Generics/Generics/MaxNumber.cs
@@ -6,24 +6,28 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    internal class MaxNumber
+    internal class MaxNumber<R> where R : IComparable
     {
-        public string FirstString, SecondString, ThirdString;
-        public string MaximumString(string FirstString, string SecondString, string ThirdString)
+        public R[] value;
+
+        public MaxNumber(R[] value)
         {
-            if (FirstString.CompareTo(SecondString) > 0 && FirstString.CompareTo(ThirdString) > 0)
-            {
-                return FirstString;
-            }
-            else if (SecondString.CompareTo(ThirdString) > 0 && SecondString.CompareTo(FirstString) > 0)
-            {
-                return SecondString;
-            }
-            else if (ThirdString.CompareTo(FirstString) > 0 && ThirdString.CompareTo(SecondString) > 0)
-            {
-                return ThirdString;
-            }
-            return FirstString;
+            this.value = value;
+        }
+        public R[] Sort(R[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public R MaxValue(R[] values)
+        {
+            var SortedValues = Sort(values);
+            return SortedValues[^1];
+        }
+        public R MaxMethod()
+        {
+            var Max = MaxValue(this.value);
+            return Max;
         }
     }
 }
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -5,9 +5,17 @@ namespace Generics
     {
         public static void Main(string[] args)
         {
-            MaxNumber maxNumber = new MaxNumber();
-            string MaximumString = maxNumber.MaximumString("Apple", "Kiwi", "Watermelon");
-            Console.WriteLine("Maximum String Value is " + MaximumString);
+            int[] intArray = { 119, 23, 63, 73, 231 };
+            MaxNumber<int> maxNumber = new MaxNumber<int>(intArray);
+            Console.WriteLine("The Maximum Integer is " + maxNumber.MaxMethod());
+
+            double[] doubleArray = { 4.55, 34.12, 56.33, 89.49, 111.71 };
+            MaxNumber<double> maxDouble = new MaxNumber<double>(doubleArray);
+            Console.WriteLine("The Maximum Float Value is " + maxDouble.MaxMethod());
+
+            string[] stringArray = { "Apple", "Grapes", "Banana", "Jackfruit" };
+            MaxNumber<string> maxString = new MaxNumber<string>(stringArray);
+            Console.WriteLine("The Maximum String Value is " + maxString.MaxMethod());
 
         }
     }
